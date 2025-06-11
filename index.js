c86e43a13e85d080fa51bb3a19ee3eb992b52f63
Draa3001 <EPEPBOOYAH1a@gmail.com>
2025-06-11 09:21:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
+
 console.log('Starting . . .')
-require('dotenv').config(), require('rootpath')(), require('./server')
-const { spawn: spawn } = require('child_process'), path = require('path'), colors = require('@colors/colors/safe'), CFonts = require('cfonts')
+require('dotenv').config()
+require('rootpath')()
+require('./server')
+
+const { spawn } = require('child_process')
+const path = require('path')
+const colors = require('@colors/colors/safe')
+const CFonts = require('cfonts')
+
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 process.on('uncaughtException', console.error)
 
 function start() {
-	let args = [path.join(__dirname, 'client.js'), ...process.argv.slice(2)]
-	let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
-	.on('message', data => {
-		if (data == 'reset') {
-			console.log('Restarting...')
-			p.kill()
-			delete p
-		}
-	})
-	.on('exit', code => {
-		console.error('Exited with code:', code)
-		start()
-	})
+    let args = [path.join(__dirname, 'client.js')]
+    let p = spawn(process.argv[0], args, { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] })
+        .on('message', data => {
+            if (data == 'reset') {
+                console.log('Restarting...')
+                p.kill()
+                delete p
+            }
+        })
+        .on('exit', code => {
+            console.error('Exited with code:', code)
+            start()
+        })
 }
 
-CFonts.say('NEOXR BOT', {
+CFonts.say('MINNIX BOT', {
    font: 'tiny',
    align: 'center',
    colors: ['system']
-}), CFonts.say('Github : https://github.com/neoxr/neoxr-bot', {
+})
+
+CFonts.say('Pairing Otomatis via Kode', {
    colors: ['system'],
    font: 'console',
    align: 'center'
-}), start()
+})
+
+start()
